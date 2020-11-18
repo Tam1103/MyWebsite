d3545a2027d697873bfc6dbdb9968718937e6108
Tam Minh <minhtam110397@gmail.com>
2020-11-18 16:03:45 +0700

    config data success

--- MyWebiste/Startup.cs
+++ MyWebiste/Startup.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MyWebiste.Models;
 
 namespace MyWebiste
 {
@@ -24,6 +22,9 @@ namespace MyWebiste
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<DatabaseContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("TechDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,11 @@ namespace MyWebiste
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("not found 4 0 4");
             });
         }
     }
