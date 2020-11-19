593a3d90edb09f9fb8bcb3a136c4e623f135daaf
Tam Minh <minhtam110397@gmail.com>
2020-11-19 17:00:07 +0700

    complete part add category

---
+++ MyWebiste/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyWebiste.Models;
+
+namespace MyWebsite.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("admin")]
+    [Route("admin/category")]
+    public class CategoryController : Controller
+    {
+        private readonly DatabaseContext db;
+        public CategoryController(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            ViewBag.categories = db.Categories.Where(c => c.Parent == null).ToList();
+            return View();
+        }
+
+        [Route("add")]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var category = new Category();
+            return View("Add", category);
+        }
+
+
+        [Route("add")]
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            category.Parent = null;
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("index", "category", new { area = "admin" });
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var categories = db.Categories.Find(id);
+            db.Categories.Remove(categories);
+            db.SaveChanges();
+            return RedirectToAction("index", "category", new { area = "admin" });
+        }
+    }
+}
