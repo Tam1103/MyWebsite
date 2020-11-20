efa757e4491a34058412f21f6737c468ccd162fe
Tam Minh <minhtam110397@gmail.com>
2020-11-20 08:52:00 +0700

    complete part post_product

---
+++ MyWebiste/Areas/Admin/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using MyWebiste.Models;
+
+namespace MyWebsite.Areas.Admin.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+        public List<SelectListItem> Categories { get; set; }
+    }
+}
