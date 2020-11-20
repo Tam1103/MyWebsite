a7f976262a08405a408c6abb38972ea602b687b3
Tam Minh <minhtam110397@gmail.com>
2020-11-20 16:09:33 +0700

    slideshow complete

---
+++ MyWebiste/ViewComponents/SlideShowViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyWebiste.Models;
+
+namespace MyWebiste.ViewComponents
+{
+    [ViewComponent(Name = "SlideShow")]
+    public class SlideShowViewComponent : ViewComponent
+    {
+        private DatabaseContext db;
+        public SlideShowViewComponent(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<SlideShow> slideshow = db.SlideShows.Where(s => s.Status).ToList();
+            return View("Index", slideshow);
+        }
+    }
+}
